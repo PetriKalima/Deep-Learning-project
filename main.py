bda997bc38de360cabe94245b7d229b9f5d4c4a0
unknown <petri.kalima@citrus.fi>
2019-05-07 17:27:12 +0300

    cont

--- main.py
+++ main.py
@@ -1,25 +1,41 @@
 import pandas as pd
 import numpy as np
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 from mlp import MLP
 from sklearn.preprocessing import StandardScaler
 
 
 def main():
     device = torch.device("cpu")
-    traindata = pd.read_csv('train.csv', delimiter=',')
+    traindata = pd.read_csv('train_house.csv', delimiter=',')
+    traindata = pd.get_dummies(traindata)
+    traindata = traindata.fillna(0)
     traintarget = traindata["SalePrice"]
     traindata_x = traindata.drop(columns="SalePrice")
     scaler = StandardScaler()
-    x_train_scaled = scaler.fit_transform(traindata_x)
-
-    mlp = MLP()
+    #x_train_scaled = scaler.fit_transform(traindata_x)
+    #print(pd.get_dummies(traindata_x))
+    mlp = MLP(289, 100, 50, 1)
     mlp.to(device)
     optimizer = torch.optim.Adam(mlp.parameters(), lr=0.005)
     n_epochs = 1000
 
+    train_accuracy_history = []
+    test_accuracy_history = []
 
-
+    for epoch in range(n_epochs):
+        x_tensor = torch.tensor(traindata_x.values, dtype=torch.float32)
+        y_tensor = torch.tensor(traintarget, dtype=torch.float32)
+        y_tensor = y_tensor.reshape(1460, 1)    
+        optimizer.zero_grad()
+        outputs = mlp.forward(x_tensor)
+        loss = F.mse_loss(outputs, y_tensor)
+        loss.backward()
+        optimizer.step()
+    print(outputs, y_tensor)    
+    print(loss)
 
 
 if __name__ == "__main__":
--- mlp.py
+++ mlp.py
@@ -2,8 +2,9 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class MLP:
+class MLP(nn.Module):
     def __init__(self, in_features, hidden_size1, hidden_size2, out_features):
+        super(MLP, self).__init__()
         self.hid1 = nn.Linear(in_features, hidden_size1)
         self.hid2 = nn.Linear(hidden_size1, hidden_size2)
         self.out = nn.Linear(hidden_size2, out_features)
